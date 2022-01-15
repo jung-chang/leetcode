d16d067de40831f5d37327588170f6dc5590f1c7
Jung <kevinjungchang@gmail.com>
2022-01-15 01:10:01 -0500

    more

--- medium/sort_list.py
+++ medium/sort_list.py
@@ -1,12 +1,16 @@
 # https://leetcode.com/problems/sort-list/
 
 from typing import Optional
+from collections import defaultdict
 
 
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+    def __repr__(self):
+        return f"{self.val}"
 
 
 class Solution:
@@ -28,31 +32,115 @@ class Solution:
             - Already sorted
             - Reversed sorted = Not sorted from the start
             - Sorted up to a point
-
-        1 2 3 N
-        3 2 1 N, 2 1 3 N, 1 2 3 N
-        2 1 3 N, 1 2 3 N
-        1 3 2 N
-
-
-        1 7 2 4 5 6 N
-        l c r
-        h
         """
         if not head:
             return None
 
+        done = False
+        while not done:
+            done = True
+            left = None
+            cur = head
+            right = cur.next
+            print(head, left, cur, right)
+            while right and cur.val <= right.val:
+                left = cur
+                cur = right
+                right = right.next
+                continue
 
-root = ListNode(6)
+            while right and cur.val > right.val:
+                print()
+                print("swap", cur, right)
+                done = False
+                temp = right.next
+                right.next = cur
+                cur.next = temp
+                if left:
+                    left.next = right
+                if cur == head:
+                    head = right
+                left = right
+                right = cur.next
+                print_list(head)
+                print(head, left, cur, right)
+        return head
 
-head = ListNode(-1)
-root.next = head
-head.next = ListNode(5)
-head.next.next = ListNode(3)
-head.next.next.next = ListNode(4)
-head.next.next.next.next = ListNode(0)
+    def second(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Use dictionary.
+        """
+        if not head:
+            return None
+        nodes = defaultdict(list)
+        cur = head
+        while cur:
+            nodes[cur.val].append(cur)
+            cur = cur.next
 
-head = Solution().sortList(root)
-while head:
-    print(head.val)
-    head = head.next
+        node_list = []
+        for key in sorted(nodes.keys()):
+            node_list.extend(nodes[key])
+
+        head = node_list[0]
+        prev = head
+        for i in range(1, len(node_list)):
+            prev.next = node_list[i]
+            prev = prev.next
+        node_list[-1].next = None
+        return head
+
+    def merge_sort(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def get_mid(node: ListNode):
+            length = 0
+            cur = node
+            while cur:
+                length += 1
+                cur = cur.next
+            cur = node
+            prev = None
+            for _ in range(length // 2):
+                prev = cur
+                cur = cur.next
+            return prev, cur, cur.next
+
+        if not head.next:
+            return head
+        left, mid, right = get_mid(head)
+
+        left.next = None
+        left_head = self.merge_sort(head)
+        right_head = self.merge_sort(right)
+
+
+def print_list(node):
+    cur = node
+    while cur:
+        print(cur, end=" ")
+        cur = cur.next
+    print()
+
+
+def create_list(vals):
+    head = None
+    prev = None
+    for val in vals:
+        node = ListNode(val)
+        if not head:
+            head = node
+        if not prev:
+            prev = node
+        else:
+            prev.next = node
+            prev = prev.next
+    return head
+
+
+head = create_list([6, 2, 7, -1, -2, 3, -2, -3])
+
+print("Original")
+print_list(head)
+
+head = Solution().sortList(head)
+print("Sorted")
+print_list(head)
